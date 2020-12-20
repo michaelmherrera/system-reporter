270c56f2205c77c69aa57923eba5ef5e2c52a731
Michael Herrera <52046844+michaelmherrera@users.noreply.github.com>
2020-12-19 21:06:14 -0800

    Before merge

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
 
 	"github.com/pkg/browser"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/mem"
 )
 
 /*
@@ -19,43 +24,40 @@ func handle(err error) {
 	}
 }
 
-// func getCPUStats() {
-// 	cpuStat, err := cpu.Info()
-// 	handle(err)
-// 	fmt.Println("CPU Model:", cpuStat[0].ModelName)
-// }
+func getCPUStats() {
+	cpuStat, err := cpu.Info()
+	handle(err)
+	fmt.Println("CPU Model:", cpuStat[0].ModelName)
+}
 
-// func getMemStats() {
-// 	memStat, err := mem.VirtualMemory()
-// 	handle(err)
-// 	fmt.Println("RAM:", memStat.Total/bytesToGig, "GB")
-// }
+func getMemStats() {
+	memStat, err := mem.VirtualMemory()
+	handle(err)
+	fmt.Println("RAM:", memStat.Total/bytesToGig, "GB")
+}
 
-// func getDiskStats() {
-// 	diskStat, err := disk.Usage("/")
-// 	handle(err)
-// 	fmt.Println("Storage:", diskStat.Total/bytesToGig, "GB")
-// }
+func getDiskStats() {
+	diskStat, err := disk.Usage("/")
+	handle(err)
+	fmt.Println("Storage:", diskStat.Total/bytesToGig, "GB")
+}
 
-// func macSystemProfiler() {
-// 	cmd := exec.Command("system_profiler", "-detailLevel", "mini", "-json", "SPHardwareDataType")
-// 	response, err := cmd.Output()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+func getMacSystemProfile() {
+	cmd := exec.Command("system_profiler", "-detailLevel", "mini", "-json", "SPHardwareDataType")
+	response, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	var testJSON map[string][](map[string]interface{})
-// 	err = json.Unmarshal(response, &testJSON)
-// 	handle(err)
-// 	info := make(map[string]interface{})
-// 	hardwareInfo := testJSON["SPHardwareDataType"][0]
-// 	info["Machine Model:"] = hardwareInfo["machine_model"]
-// 	info["Memory:"] = hardwareInfo["physical_memory"]
-// 	info["CPU Type:"] = hardwareInfo["cpu_type"]
-// 	for k := range info {
-// 		fmt.Println(k, info[k])
-// 	}
-// }
+	var testJSON map[string][](map[string]interface{})
+	err = json.Unmarshal(response, &testJSON)
+	handle(err)
+	info := make(map[string]interface{})
+	hardwareInfo := testJSON["SPHardwareDataType"][0]
+	info["Machine Model:"] = hardwareInfo["machine_model"]
+	info["Memory:"] = hardwareInfo["physical_memory"]
+	info["CPU Type:"] = hardwareInfo["cpu_type"]
+}
 
 func serveUp() {
 
@@ -72,8 +74,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(data))
 }
 
+func dataEndpoint(w http.ResponseWriter, r *http.Request) {
+	macSystemProfiler
+	json.NewEncoder(w).Encode(arr)
+}
+
+func restCalls() {
+	fs := http.FileServer(http.Dir("."))
+	http.Handle("/", fs)
+	http.HandleFunc("/data.json", dataEndpoint)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
 func main() {
-	serveUp()
+	restCalls()
+	//serveUp()
 	// macSystemProfiler()
 	// getCPUStats()
 	// getMemStats()
